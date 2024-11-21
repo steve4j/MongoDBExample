a05418e1d5f856c392d9ce53298b846b2d3103fc
stefan.mueller@issp.cc <stefan.mueller@issp.cc>
2024-11-21 09:41:40 +0100

    Datenblätter Index

---
+++ PdfExtractor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UglyToad.PdfPig;
+using UglyToad.PdfPig.Content;
+
+namespace MongoDBIndexer
+{
+    #region PdfExtractor class
+
+    public class PdfExtractor : IDisposable
+    {
+        private PdfDocument pdfDocument;
+
+        public PdfExtractor(string fileName)
+        {
+            pdfDocument = PdfDocument.Open(fileName);
+        }
+
+        public void Dispose()
+        {
+            if (pdfDocument != null)
+            {
+                pdfDocument.Dispose();
+                pdfDocument = null;
+            }
+        }
+
+        public string ExtractText()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (Page page in pdfDocument.GetPages())
+            {
+                string pageText = page.Text;
+                    sb.AppendLine(pageText);
+            }
+
+            return sb.ToString();
+        }
+    }
+
+    #endregion
+}
